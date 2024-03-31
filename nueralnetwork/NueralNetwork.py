209b6ade790b439566fedf9a6f4dac08793d2558
ivan-yung <120080144+ivan-yung@users.noreply.github.com>
2024-03-30 17:04:53 -0700

    implimented backpropagation functions
    Finished implimenting backpropagation functions, allowing the NN to learn via updates through weights and biases

--- nueralnetwork/NueralNetwork.py
+++ nueralnetwork/NueralNetwork.py
@@ -14,11 +14,27 @@ class Layer_Dense:
     def forward(self, inputs):
         #forward is the dot product
         self.output = np.dot(inputs, self.weights) + self.biases
+        
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradient on values
+        self.dvalues = np.dot(dvalues, self.weights.T)
+        
+    def update(self, learning_rate):
+        self.weights -= learning_rate * self.dweights
+        self.biases -= learning_rate * self.dbiases
+
 
 class Activation_ReLU:
     def forward(self, inputs):
         #returns input if its greater than 0
         self.output = np.maximum(0, inputs)
+    def backward(self, dvalues):
+        self.dvalues = dvalues.copy()  # Copy dvalues to avoid overwriting original array
+        self.dvalues[self.inputs <= 0] = 0  # Derivative of ReLU
 
 #softmax is used on ouput layer to determine
 #the probability of a guess/ outcome
@@ -30,12 +46,26 @@ class Activation_Softmax:
         #normalize values with formula, value/sum of all values
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
+        
+    def backward(self, dvalues):
+        self.dvalues = dvalues.copy()  # Copy dvalues to avoid overwriting original array
 
+    # No need to implement update method for Activation layers
 class Loss:
     def calculate(self, output, y):
         sample_losses = self.forward(output, y)
         data_loss = np.mean(sample_losses)
         return data_loss
+        
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        self.dvalues = self.calculate_dvalues(dvalues, y_true) / samples
+
+
+    # Placeholder method for calculating dvalues
+    def calculate_dvalues(self, dvalues, y_true):
+        raise NotImplementedError("Subclasses must implement calculate_dvalues method")
+    
 
 class Loss_CategoricalCrossentropy(Loss):
     def forward(self, y_pred, y_true):
@@ -50,7 +80,18 @@ class Loss_CategoricalCrossentropy(Loss):
 
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+
     
+    def calculate_dvalues(self, dvalues, y_true):
+        # Convert class labels to one-hot encoded vectors if they're not already
+        if len(y_true.shape) == 1:
+            y_true = np.eye(dvalues.shape[1])[y_true]
+
+        # Calculate gradient
+        self.dvalues = -(y_true - dvalues)
+        return self.dvalues
+
+
 
 
 X,y = spiral_data(samples=100, classes = 3)
